e5c7886005bd506793019fb64a1bbb09b958463f
narekatturohan-sketch <narekattu.rohan@gmail.com>
2026-06-28 17:45:35 +0530

    feat: Added service layer

---
+++ src/main/com/example/demo/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+package com.example.demo.model.Customer;
+
+import com.example.demo.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository repository;
+    public CustomerService(CustomerRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Customer> getCustomers(){
+        return repository.findAll();
+    }
+}
